0f79d7fbafef5934585d26e02f587c68eadee2cf
JangJaeHyung1 <ggassoon@naver.com>
2021-03-31 14:26:11 +0900

    장소 메세지를 탭했을때 mapkit을 띄움

--- Messenger/Controllers/ChatViewController.swift
+++ Messenger/Controllers/ChatViewController.swift
@@ -152,7 +152,8 @@ class ChatViewController: MessagesViewController {
     }
     
     private func presentLocationPicker(){
-        let vc = LocationPickerViewController()
+        let vc = LocationPickerViewController(coordinates: nil)
+        vc.title = "Pick Location"
         
         vc.navigationItem.largeTitleDisplayMode = .never
         
@@ -449,7 +450,21 @@ extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, Messag
 
 
 extension ChatViewController: MessageCellDelegate{
-    
+    func didTapMessage(in cell: MessageCollectionViewCell) {
+        guard let indexPath = messagesCollectionView.indexPath(for: cell) else {
+            return
+        }
+        let message = messages[indexPath.section]
+        switch message.kind {
+        case .location(let locationData):
+            let coordinate = locationData.location.coordinate
+            let vc = LocationPickerViewController(coordinates: coordinate)
+            vc.title = "Location"
+            self.navigationController?.pushViewController(vc, animated: true)
+        default:
+            break
+        }
+    }
     
     
     func didTapImage(in cell: MessageCollectionViewCell) {
--- Messenger/Controllers/LocationPickerViewController.swift
+++ Messenger/Controllers/LocationPickerViewController.swift
@@ -14,29 +14,50 @@ class LocationPickerViewController: UIViewController {
     public var completion: ((CLLocationCoordinate2D) -> Void)?
     
     private var coordinates: CLLocationCoordinate2D?
-    
+    private var isPickable = true
     private let map: MKMapView = {
         let map = MKMapView()
         return map
     }()
     
+    init(coordinates: CLLocationCoordinate2D?){
+        self.coordinates = coordinates
+        self.isPickable = false
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Pick Location"
         view.backgroundColor = .systemBackground
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Send", style: .done, target: self, action: #selector(sendButtonTapped))
+        if isPickable{
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Send", style: .done, target: self, action: #selector(sendButtonTapped))
+            
+            map.isUserInteractionEnabled = true
+            
+            let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapMap(_:)))
+            
+            gesture.numberOfTapsRequired = 1
+            gesture.numberOfTouchesRequired = 1
+            
+            map.addGestureRecognizer(gesture)
+        }
+        else{
+            // just showing location
+            guard let coordinates = self.coordinates else { return }
+            
+            let pin = MKPointAnnotation()
+            pin.coordinate = coordinates
+            
+            map.addAnnotation(pin)
+            
+        }
         
         view.addSubview(map)
         
-        map.isUserInteractionEnabled = true
-        
-        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapMap(_:)))
-        
-        gesture.numberOfTapsRequired = 1
-        gesture.numberOfTouchesRequired = 1
-        
-        map.addGestureRecognizer(gesture)
     }
     
     @objc func sendButtonTapped(){
